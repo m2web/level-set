19d7bbba7e10b9986e61309c6f1fb80323317a88
Mark McFadden <m2web@yahoo.com>
2012-01-28 10:31:44 -0500

    initial commit of the very base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Levelset::Application.config.secret_token = '3b052689786e9a8c9b3c80354f29feac32650d9f347990327ffd7bb0aa7b539f9c5df90b9c2a72e538628e155c869c940127585a18909d18ef9696fc06ada934'
