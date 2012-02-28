7b56a5b9cce8f5bdb7aa56384d9f0b7ae97de772
Mark McFadden <m2web@yahoo.com>
2012-02-27 20:24:50 -0500

    added gravitar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 	def gravatar_for(user, options = { :size => 75 })
-    gravatar_image_tag(user.email.downcase, :alt => h(user.first_name),
+    gravatar_image_tag(user.email.downcase, :alt => h(user.first_name) + " " +  h(user.last_name),
                                             :class => 'gravatar',
                                             :gravatar => options)
   end
